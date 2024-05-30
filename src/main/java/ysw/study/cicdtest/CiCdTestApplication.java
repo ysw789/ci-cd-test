4d87b268d37e1dbeffc1dda238db1f6fb8503cb4
Seungwan Yoo <ysw99@kakao.com>
2024-05-30 12:52:12 +0900

    first commit for ci/cd test

---
+++ src/main/java/ysw/study/cicdtest/CiCdTestApplication.java
@@ -0,0 +1,13 @@
+package ysw.study.cicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ysw/study/cicdtest/CiCdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ysw.study.cicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

5490b018707c2e546561cac063912d1a84da5da1
Seungwan Yoo <ysw99@kakao.com>
2024-06-29 13:03:22 +0900

    AWS Secrets Manager Injection Test

--- src/main/java/ysw/study/cicdtest/CiCdTestApplication.java
+++ src/main/java/ysw/study/cicdtest/CiCdTestApplication.java
@@ -1,13 +1,23 @@
 package ysw.study.cicdtest;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CiCdTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CiCdTestApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner(@Value("${my-first-secret}") String myFirstSecret) {
+        return args -> {
+            System.out.println("ASM inject test: " + myFirstSecret);
+        };
     }
 
 }
